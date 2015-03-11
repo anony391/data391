4c1de6522ee50aaaf13c3f38284e1ea85943cd06
rjynn <rdejesus@cs.ualberta.ca>
2015-03-11 00:40:56 -0600

    took out sql java code from jsp file into a java file for more modularity. better code

--- WEB-INF/classes/connectionmaker/connmaker.java
+++ WEB-INF/classes/connectionmaker/connmaker.java
@@ -1,10 +1,11 @@
-/*This code is for making the connection to oracle
+/*This code is for making the connection to oracle and sending and retrieving information about login data
 */
 package connectionmaker;
 import java.sql.*;
 import java.io.*;
+import java.io.IOException;
 
-
+/*This method creates a connection to connect to oracle*/
 public class connmaker{
   String USER = ""; //Change these parameters when testing to your oracle password :)
   String PASSWORD = "";
@@ -22,5 +23,40 @@ public class connmaker{
     catch(Exception ex){
       return null;
     }
-}}		
+  }
+
+/*This method takes the parameters and sends statements to SQL to retrieve any login info to see if user can log in.
+*/
+  public boolean logincheck(String user, String password, Connection conn) throws IOException, SQLException{
+    Statement stmt = null;
+    ResultSet rset = null;
+    String sqlstring = "SELECT password FROM users WHERE user_name = '"+user+"'";
+    try{
+      stmt = conn.createStatement();
+      rset = stmt.executeQuery(sqlstring);
+    }
+    catch(Exception ex){
+      conn.close();
+      throw new IOException("Cannot retrieve information from database server.");
+
+    }
+    String dbpassword = "";  	//checking if results includes logininfo
+    while(rset != null && rset.next()){
+        dbpassword = rset.getString(1).trim();
+        if(password.equals(dbpassword)){ 	//need to change this to going to nextpage
+	    conn.close();
+	    return true;
+        }
+        else{
+	    conn.close();
+            return false;
+        }
+    }
+    return false;
+}}
+
+
+
+
+
 
